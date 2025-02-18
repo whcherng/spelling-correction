18079827e6b15211839164d1bfe4fff1446ec86b
Wong Hau Cherng <metalfullwong@hotmail.com>
2025-02-19 03:15:16 +0800

    Add text area

--- src/app/spelling-correction/page.tsx
+++ src/app/spelling-correction/page.tsx
@@ -1,7 +1,19 @@
 import {NextPage} from "next";
 
 const SpellingCorrectionPage: NextPage = () => {
-    return (<div>Spelling Correctiong</div>);
+    return (
+        <div className="flex gap-6 h-svh p-[12px]">
+            <div className="w-full h-full">
+                <h1>Spelling Correction system</h1>
+                <textarea
+                    className="w-full h-5/6 p-4 mt-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Enter your text here..."
+                />
+            </div>
+            <div className="w-[400px]">
+                <h1>Assistant tab</h1>
+            </div>
+        </div>);
 }
 
 export default SpellingCorrectionPage;
